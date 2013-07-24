9ebc88064968507b4777a633cec99d3463aacb17
Marshall Shen <mshen@groupon.com>
2013-07-24 16:15:11 -0500

    fix file directory

--- lib/dextery/generator.rb
+++ lib/dextery/generator.rb
@@ -4,11 +4,10 @@ module Dextery
       raise ArgumentError.new("You must specify a path") if args.empty?
 
       new_blog_path = File.expand_path(args.join(" "), Dir.pwd)
-      FileUtils.mkdir_p new_blog_path
       if preserve_source_location?(new_blog_path, options)
         Dextery.logger.error "Conflict:", "#{new_blog_path} exists and is not empty."
       else
-        clean_directory(new_blog_path)
+        clean_directory(new_blog_path) if options[:force]
         copy_bootstrap(new_blog_path)
         notify_success
       end
@@ -16,18 +15,19 @@ module Dextery
 
     private
     def self.preserve_source_location?(path, options)
-      !options[:force] && !Dir["#{path}/**/*"].empty?
+      options[:force] && !Dir["#{path}/**/*"].empty?
     end
 
     def self.clean_directory(path)
-      Dextery.logger.info "Check directory #{path}.. Start"
+      Dextery.logger.info "Cleanup directory #{path}.. Start"
       system("rm -rf #{path}")
-      Dextery.logger.info "Check directory #{path}.. Complete"
+      Dextery.logger.info "Cleanup directory #{path}.. Complete"
     end
 
     def self.copy_bootstrap(path)
+      template_path = File.join(File.expand_path('..', __FILE__), 'template', '/')
       Dextery.logger.info "Setup blog using bootstrap.. Start"
-      system( "cp -r lib/dextery/template/ #{path}")
+      system( "cp -r #{template_path} #{path}")
       Dextery.logger.info "Setup blog using bootstrap.. Complete"
     end
 
